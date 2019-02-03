13c300457ae3d3751f79396dd5eb1f451d606dcf
Abhishekyagami <abhishekyagami2@gmail.com>
2019-02-03 18:31:12 +0530

    first commit

---
+++ sketch.js
@@ -0,0 +1,50 @@
+let bird;
+let pipes = [];
+let bg;
+
+function setup() {
+  bg = loadImage('img/gree.jpg');
+
+  createCanvas(600, 400);
+ 
+  bird = new Bird();
+  pipes.push(new Pipe());
+}
+
+function draw() {
+  background(23);
+  //background(bg);
+
+  for (let i = pipes.length-1; i >= 0; i--) {
+    pipes[i].show();
+    pipes[i].update();
+
+    if (pipes[i].hits(bird)) {
+      console.log("HIT");
+    }
+
+
+    if (pipes[i].offscreen()) {
+      pipes.splice(i, 1);
+    }
+
+
+  }
+
+  bird.update();
+  bird.show();
+
+  if (frameCount % 100 == 0) {
+    pipes.push(new Pipe());
+  }
+
+
+
+}
+
+function keyPressed() {
+  if (key == ' ') {
+    bird.up();
+    //console.log("SPACE");
+  }
+}
